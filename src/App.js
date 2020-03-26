f6b812e3531f018a9b6aa17b14140c4d8ea9ac0f
Hasan_sh <hasansh343@gmail.com>
2020-03-26 09:35:02 +0100

    Removed unneeded code

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import { getColumns, icons } from './constants';
 import Map from './Map';
 
 function fetch_data(countries) {
-  //retrain countries ? covid.countries() : covid.all();
-  console.log(covid);
   return countries ? covid.getCountry() : covid.getAll();
 }
 
 function AllCases(props) {
-  console.log(props.all);
   return (
     <MaterialTable
       title="Cases overall"
@@ -60,32 +57,6 @@ function App() {
       </div>
       <Countries countries={data.countries} />
       <button onClick={load}>Reload</button>
-      {/* {data.countries &&
-       (<table>
-            <thead><tr>
-                <th>Country</th>
-                <th>Cases</th>
-                <th>Cases Today</th>
-                <th>Deaths</th>
-                <th>Deaths Today</th>
-                <th>Recovered</th>
-                <th>Currently Active</th>
-            </tr></thead>
-        {data.countries
-         .map(country => {
-           return <tbody><tr>
-                    <td>{country.country}</td>
-                    <td>{country.cases}</td>
-                    <td>{country.todayCases}</td>
-                    <td>{country.deaths}</td>
-                    <td>{country.todayDeaths}</td>
-                    <td>{country.recovered}</td>
-                    <td>{country.active}</td>
-                </tr></tbody>
-         })
-        }
-        </table>)
-      } */}
     </div>
   );
 }
