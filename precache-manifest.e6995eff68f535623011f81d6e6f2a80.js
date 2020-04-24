7e0bcf81f84c78bdcf9a69843998fc6b35b6ce0b
Hasan_sh <hasansh343@gmail.com>
2020-04-24 09:12:37 +0200

    Updates

---
+++ precache-manifest.e6995eff68f535623011f81d6e6f2a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1167b5b7c1d5ded68c7a08039146699",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "4736b91916ed536175cb",
+    "url": "/corona/static/css/main.ca0f7172.chunk.css"
+  },
+  {
+    "revision": "a2bddaf6c0718be7e0c2",
+    "url": "/corona/static/js/2.cda0d523.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/corona/static/js/2.cda0d523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4736b91916ed536175cb",
+    "url": "/corona/static/js/main.31a2558c.chunk.js"
+  },
+  {
+    "revision": "a3b787bfbf2c81f2e0d4",
+    "url": "/corona/static/js/runtime-main.c6168e55.js"
+  }
+]);
